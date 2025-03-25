9934a285202de1611acc602bc7e0324ce2933742
stilo3 <tadie3088@gmail.com>
2025-03-25 09:51:11 +0000

    debug

--- eld_project/settings.py
+++ eld_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0c0h(#)!lcv6lawtqjuxqh5uzcunsa^xuoc83y^ejmqaai@qfg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
